bc65f8ca5b9dec414069ce508513d7e1fcd90f9a
koyama <small6mountain@gmail.com>
2025-05-08 00:41:03 +0900

    テスト新規ファイル

---
+++ lambda/test2.py
@@ -0,0 +1,19 @@
+
+import json
+
+def lambda_handler(event, context):
+    """
+    Lambda関数のエントリーポイント
+    """
+    print("イベントを受信しました:", event)
+    print("heyhey")
+
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({
+            'message': 'Hello from Lambda!',
+            'input': event
+        })
+    }
+
+    return response
